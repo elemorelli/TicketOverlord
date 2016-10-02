0950180b8c11b1f150a6b83eb8a1fbc770b434eb
Eleazar Morelli <gemorelli@gmail.com>
2016-10-02 18:29:14 +0200

    Main renaming

--- src/es/ujaen/dae/ticketlord/SpringMain.java
+++ src/es/ujaen/dae/ticketlord/SpringMain.java
@@ -5,7 +5,7 @@ import es.ujaen.dae.ticketlord.services.VentaTicketsService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class main {
+public class SpringMain {
     public static void main(String[] args) {
 
         ApplicationContext appContext = new
