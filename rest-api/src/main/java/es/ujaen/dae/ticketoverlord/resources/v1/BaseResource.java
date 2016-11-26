0ca76a70b262d05f4b8e6fcafbf9744a2de4eb5c
Eleazar Morelli <gemorelli@gmail.com>
2016-11-26 20:28:35 +0100

    Adding Login and Logout endpoints

--- rest-api/src/main/java/es/ujaen/dae/ticketoverlord/resources/v1/BaseResource.java
+++ rest-api/src/main/java/es/ujaen/dae/ticketoverlord/resources/v1/BaseResource.java
@@ -1,7 +1,9 @@
 package es.ujaen.dae.ticketoverlord.resources.v1;
 
-import es.ujaen.dae.ticketoverlord.services.UsersService;
+import es.ujaen.dae.ticketoverlord.dtos.UserDTO;
+import es.ujaen.dae.ticketoverlord.services.SessionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +18,27 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 @RequestMapping(API)
 public class BaseResource {
     @Autowired
-    private UsersService usersService;
+    private SessionService sessionService;
     final static String API = "/api/v1";
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public Map<String, String> getHomeLinks() throws NoSuchMethodException {
         Map<String, String> links = new HashMap<>();
 
+        links.put("Login", linkTo(BaseResource.class.getMethod("login", UserDTO.class), 2L).withSelfRel().getHref());
+        links.put("Logout", linkTo(BaseResource.class.getMethod("logout", UserDTO.class), 2L).withSelfRel().getHref());
         links.put("Users", linkTo(UsersResource.class.getMethod("getUsers"), 2L).withSelfRel().getHref());
 
         return links;
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/login")
+    public UserDTO login(@RequestBody UserDTO userDTO) {
+        return sessionService.authenticateUser(userDTO);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/logout")
+    public void logout(@RequestBody UserDTO userDTO) {
+        sessionService.logoutUser(userDTO);
     }
 }
