43e91931306be05b41aada9d95373281341c3084
Eleazar Morelli <gemorelli@gmail.com>
2016-12-10 22:05:40 +0100

    Basic Spring Security config

---
+++ rest-api/src/main/java/es/ujaen/dae/ticketoverlord/configurations/AppSecurityInitializer.java
@@ -0,0 +1,6 @@
+package es.ujaen.dae.ticketoverlord.configurations;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class AppSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ rest-api/src/main/java/es/ujaen/dae/ticketoverlord/configurations/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package es.ujaen.dae.ticketoverlord.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username, password, enabled FROM users WHERE username=?")
+                .authoritiesByUsernameQuery("SELECT username, role FROM users WHERE username=?")
+        ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+        ;
+    }
+}
